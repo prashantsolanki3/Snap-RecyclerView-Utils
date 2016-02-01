e6ab529eac6f0fc37c5e73f6fa037f3a672406de
Prashant Solanki <solankisrp2@gmail.com>
2016-02-01 18:10:32 +0530

    Size log removed

--- snaplibrary/src/main/java/io/github/prashantsolanki3/snaplibrary/snap/adapter/AbstractSnapMultiAdapter.java
+++ snaplibrary/src/main/java/io/github/prashantsolanki3/snaplibrary/snap/adapter/AbstractSnapMultiAdapter.java
@@ -6,7 +6,6 @@ import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -236,8 +235,6 @@ public abstract class AbstractSnapMultiAdapter<T> extends RecyclerView.Adapter<S
     @Override
     public int getItemCount() {
         int size = mData.size();
-        Log.d("Size", size + "");
-
         if (autoEmptyLayoutHandling)
             if (alternateViewContainer != null && size == 0)
                 showEmptyLayout();
